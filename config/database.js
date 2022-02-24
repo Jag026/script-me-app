ac4bdc3e95dc087f75321a9e13762d9a114223c9
Drew <DGriffin@appacademy.io>
2022-02-23 21:05:28 -0600

    4

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ const db = config.db;
 const username = db.username;
 const password = db.password;
 const database = db.database;
-const sessionSecret = db.sessionSecret;
 const host = db.host;
 
 module.exports = {
